b47e7a3c91a6fbe49a0d1c1c81b02508e5fb5169
okasyun <shunshun1207shun@gmail.com>
2025-04-05 20:50:08 +0900

    環境ごとに変わる値を返す関数を作成

--- infrastracture/bin/ecs-practice.ts
+++ infrastracture/bin/ecs-practice.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { EcsPracticeStack } from "../lib/ecs-practice-stack";
+import { getStackProps } from "../lib/config";
 
 const app = new cdk.App();
-new EcsPracticeStack(app, "EcsPracticeStack");
+const env = process.env.ENV || "Dev";
+const stackProps = getStackProps(env);
+new EcsPracticeStack(app, "EcsPractice", stackProps);
---
+++ infrastracture/lib/config.ts
@@ -0,0 +1,20 @@
+import { StackProps } from "aws-cdk-lib";
+
+export const getStackProps = (stage: string): StackProps => {
+  switch (stage) {
+    case "Prod":
+      return {
+        env: {
+          account: "637423478672",
+          region: "ap-northeast-1",
+        },
+      };
+    default:
+      return {
+        env: {
+          account: "637423478672",
+          region: "ap-northeast-1",
+        },
+      };
+  }
+};
